3cad059c0891e1b1c39f311f9957771d1b91f9be
Even Alander <even@alander.org>
2016-01-16 23:13:29 +0100

    main game rough layout and start of the piece class

---
+++ src/tetra.js
@@ -0,0 +1,71 @@
+"use strict";
+
+export default class Tetra {
+	constructor(shape) {
+		this.shape = null;
+
+		switch(shape) {
+		case 'I':
+			this.color = 'cyan';
+			this.shape = [
+				[1, 1, 1, 1],
+				[0, 0, 0, 0],
+			];
+			break;
+		case 'J':
+			this.color = 'blue';
+			this.shape = [
+				[1, 1, 1, 0],
+				[0, 0, 1, 0],
+			];
+			break;
+		case 'L':
+			this.color = 'orange';
+			this.shape = [
+				[1, 1, 1, 0],
+				[1, 0, 0, 0],
+			];
+			break;
+		case 'O':
+			this.color = 'yellow';
+			this.shape = [
+				[1, 1, 0, 0],
+				[1, 1, 0, 0],
+			];
+			break;
+		case 'S':
+			this.color = 'lime';
+			this.shape = [
+				[0, 1, 1, 0],
+				[1, 1, 0, 0],
+			];
+			break;
+		case 'T':
+			this.color = 'purple';
+			this.shape = [
+				[1, 1, 1, 0],
+				[0, 1, 0, 0],
+			];
+			break;
+		case 'Z':
+			this.color = 'red';
+			this.shape = [
+				[1, 1, 0, 0],
+				[0, 1, 1, 0],
+			];
+			break;
+		}
+	}
+
+	rotate() {
+
+	}
+
+	update() {
+
+	}
+
+	render() {
+		
+	}
+}
---
+++ src/tetris.js
@@ -0,0 +1,13 @@
+"use strict";
+
+import Tetra from './tetra';
+import AnimationFrame from 'animation-frame';
+
+let pieces = [];
+
+const animationFrame = new AnimationFrame();
+const gameLoop = () => {
+	animationFrame.request(gameLoop);
+}
+
+animationFrame.request(gameLoop);
